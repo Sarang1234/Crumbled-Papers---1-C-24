1d28abebfd71a2043afd1d326103d495654a82eb
Sarang1234 <83072803+Sarang1234@users.noreply.github.com>
2021-05-14 16:58:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,9 +18,9 @@ function setup() {
 
   ground = new Ground(400, 680, 800, 20);
   
-  leftSide = new Dustbin(550, 620, 20, 100);
-  bottom = new Dustbin(610, 660, 100, 20);
-  rightSide = new Dustbin(670, 620, 20, 100);
+  leftSide = new Dustbin(525, 605, 20, 100);
+  bottom = new Dustbin(610, 660, 150, 20);
+  rightSide = new Dustbin(695, 605, 20, 100);
 
 	Engine.run(engine);
   
